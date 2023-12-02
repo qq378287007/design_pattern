022ce4ce7c0ed1b65293b30e6c8675949914ef71
qq378287007 <qq378287007@163.com>
2023-12-02 12:09:04 +0800

    add

---
+++ 解释器模式.cpp
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+#include <cstring>
+#include <memory>
+#include <set>
+using namespace std;
+
+// 定义一个语言的文法（语法规则），并建立一个解释器解释该语言中的句子
+// 4种角色
+// AbstractExpression（抽象表达式），声明抽象的解释操作
+// TerminalExpression（终结符表达式），实现语言文法中与终结表达式相关的解释操作
+// NonterminalExpression（非终结符表达式），实现语言文法中与非终结表达式相关的解释操作
+// Context（环境类/上下文类），存储解释器之外的全局信息，变量名与值的映射关系、存储和访问表达式解释器的状态等，作为公共对象的参数传递到表达式的解释操作中
+
+namespace ns1
+{
+    class Expression // 小表达式（节点）父类
+    {
+    public:              // 以下两个成员变量是为程序跟踪调试时观察某些数据方便而引入
+        int m_dbg_num;   // 创建该对象时的一个编号，用于记录本对象是第几个创建的
+        char m_dbg_sign; // 标记本对象的类型，可能是个字符v代表变量（终结符表达式），也可能是个加减号（非终结符表达式）
+    public:
+        Expression(int num, char sign) : m_dbg_num(num), m_dbg_sign(sign) {}
+        virtual ~Expression() {}
+
+    public:                                             // 解析语法树中的当前节点
+        virtual int interpret(map<char, int> &var) = 0; // 变量名及对应的值
+    };
+
+    class VarExpression : public Expression // 变量表达式（终结符表达式）
+    {
+        char m_key; // 变量名，本范例中诸如a、b、c、d都是变量名
+    public:
+        VarExpression(const char &key, int num, char sign) : Expression(num, sign), m_key(key) {}
+        int interpret(map<char, int> &var) override { return var[m_key]; } // 返回变量名对应的数值
+    };
+
+    class SymbolExpression : public Expression // 运算符表达式（非终结符表达式）父类
+    {
+    protected: // 左右各有一个操作数
+        shared_ptr<Expression> m_left;
+        shared_ptr<Expression> m_right;
+
+    public:
+        SymbolExpression(const shared_ptr<Expression> &left, const shared_ptr<Expression> &right, int num, char sign) : m_left(left), m_right(right), Expression(num, sign) {}
+        shared_ptr<Expression> getLeft() const { return m_left; }
+        shared_ptr<Expression> getRight() const { return m_right; }
+    };
+
+    class AddExpression : public SymbolExpression // 加法运算符表达式（非终结符表达式）
+    {
+    public:
+        AddExpression(const shared_ptr<Expression> &left, const shared_ptr<Expression> &right, int num, char sign) : SymbolExpression(left, right, num, sign) {} // 构造函数
+
+        int interpret(map<char, int> &var) override
+        {
+            // 分步骤拆开写，方便理解和观察
+            int value1 = m_left->interpret(var);  // 递归调用左操作数的interpret方法
+            int value2 = m_right->interpret(var); // 递归调用右操作数的interpret方法
+            int result = value1 + value2;
+            return result; // 返回两个变量相加的结果
+        }
+    };
+
+    class SubExpression : public SymbolExpression // 减法运算符表达式（非终结符表达式）
+    {
+    public:
+        SubExpression(const shared_ptr<Expression> &left, const shared_ptr<Expression> &right, int num, char sign) : SymbolExpression(left, right, num, sign) {} // 构造函数
+
+        int interpret(map<char, int> &var) override
+        {
+            int value1 = m_left->interpret(var);
+            int value2 = m_right->interpret(var);
+            int result = value1 - value2;
+            return result; // 返回两个变量相减的结果
+        }
+    };
+
+    // 分析—创建语法树（表达式树）
+    shared_ptr<Expression> analyse(const string &strExp) // strExp：要计算结果的表达式字符串，比如"a-b+c+d"
+    {
+        stack<shared_ptr<Expression>> expStack; // #include <stack>，这里用到了栈这种顺序容器
+        shared_ptr<Expression> left;
+        shared_ptr<Expression> right;
+        int icount = 1;
+        for (size_t i = 0; i < strExp.size(); ++i) // 循环遍历表达式字符串中的每个字符
+        {
+            switch (strExp[i])
+            {
+            case '+':                  // 加法运算符表达式（非终结符表达式）
+                left = expStack.top(); // 返回栈顶元素（左操作数）
+                ++i;
+                right.reset(new VarExpression(strExp[i], icount++, 'v')); // v代表是个变量节点
+                // 在栈顶增加元素
+                expStack.push(make_shared<AddExpression>(left, right, icount++, '+')); //'+'代表是个减法运算符节点
+                break;
+            case '-':                  // 减法运算符表达式（非终结符表达式）
+                left = expStack.top(); // 返回栈顶元素
+                ++i;
+                right.reset(new VarExpression(strExp[i], icount++, 'v'));
+                expStack.push(make_shared<SubExpression>(left, right, icount++, '-')); //'-'代表是个减法运算符节点
+                break;
+            default: // 变量表达式（终结符表达式）
+                expStack.push(make_shared<VarExpression>(strExp[i], icount++, 'v'));
+                break;
+            }
+        }
+        shared_ptr<Expression> expression = expStack.top(); // 返回栈顶元素
+        return expression;
+    }
+}
+
+namespace ns2
+{
+    class Expression // 表达式父类
+    {
+    public:
+        virtual ~Expression() {}
+
+    public: // 解析语法树中的当前节点
+        virtual string interpret() = 0;
+    };
+
+    class DirectionExpression : public Expression // 运动方向表达式（终结符表达式）
+    {
+        string m_direction; // 运动方向：up、down、left、right分别表示上、下、左、右
+    public:
+        DirectionExpression(const string &direction) : m_direction(direction) {}
+        string interpret() override
+        {
+            static set<string> directionSet = {"up", "down", "left", "right"};
+            if (directionSet.find(m_direction) != directionSet.end())
+                return m_direction;
+            else
+                return "direction error";
+        }
+    };
+
+    class ActionExpression : public Expression // 运动方式表达式（终结符表达式）
+    {
+        string m_action; // 运动方式：walk、run分别表示行走、奔跑
+    public:
+        ActionExpression(const string &action) : m_action(action) {}
+        string interpret() override
+        {
+            static set<string> actionSet = {"walk", "run"};
+            if (actionSet.find(m_action) != actionSet.end())
+                return m_action;
+            else
+                return "action error";
+        }
+    };
+
+    class DistanceExpression : public Expression // 运动距离表达式（终结符表达式）
+    {
+        string m_distance; // 运动距离，用字符串表示即可
+    public:
+        DistanceExpression(const string &distance) : m_distance(distance) {}
+        string interpret() override
+        {
+            return m_distance + "m";
+        }
+    };
+
+    class SentenceExpression : public Expression // “句子”表达式（非终结符表达式）,“运动方向 运动方式 运动距离”构成
+    {
+        shared_ptr<Expression> m_direction; // 运动方向
+        shared_ptr<Expression> m_action;    // 运动方式
+        shared_ptr<Expression> m_distance;  // 运动距离
+    public:
+        SentenceExpression(const shared_ptr<Expression> &direction, const shared_ptr<Expression> &action, const shared_ptr<Expression> &distance)
+            : m_direction(direction), m_action(action), m_distance(distance) {}
+        shared_ptr<Expression> getDirection() const { return m_direction; }
+        shared_ptr<Expression> getAction() const { return m_action; }
+        shared_ptr<Expression> getDistance() const { return m_distance; }
+
+        string interpret() override
+        {
+            return m_direction->interpret() + " " + m_action->interpret() + " " + m_distance->interpret();
+        }
+    };
+
+    class AndExpression : public Expression // “和”表达式（非终结符表达式）
+    {
+        shared_ptr<Expression> m_left;
+        shared_ptr<Expression> m_right;
+
+    public:
+        AndExpression(const shared_ptr<Expression> &left, const shared_ptr<Expression> &right) : m_left(left), m_right(right) {}
+        shared_ptr<Expression> getLeft() const { return m_left; }
+        shared_ptr<Expression> getRight() const { return m_right; }
+
+        string interpret() override
+        {
+            return m_left->interpret() + " and " + m_right->interpret();
+        }
+    };
+
+    // 分析—创建语法树（表达式树）
+    shared_ptr<Expression> analyse(const string &strExp) // strExp：要计算结果的表达式字符串，比如"left walk 15 and down run 20"
+    {
+        stack<shared_ptr<Expression>> expStack;
+        shared_ptr<Expression> direction;
+        shared_ptr<Expression> action;
+        shared_ptr<Expression> distance;
+        shared_ptr<Expression> left;
+        shared_ptr<Expression> right;
+
+        // 机器人运动控制命令之间是用空格来分隔的，所以用空格作为分隔字符来对整个字符串进行拆分
+        char *strc = new char[strlen(strExp.c_str()) + 1];
+        strcpy(strc, strExp.c_str());     // 若本行编译报错提醒使用strcpy_s，则可以在文件头增加代码行：#pragma warning(disable : 4996)
+        vector<string> resultVec;         // #include <vector>
+        char *tmpStr = strtok(strc, " "); // 按空格来切割字符串
+        while (tmpStr != nullptr)
+        {
+            resultVec.push_back(string(tmpStr));
+            tmpStr = strtok(NULL, " ");
+        }
+        delete[] strc;
+        for (auto iter = resultVec.begin(); iter != resultVec.end(); ++iter)
+        {
+            if ((*iter) == "and") // 和
+            {
+                left = expStack.top(); // 返回栈顶元素（左操作数）
+                ++iter;
+
+                direction.reset(new DirectionExpression(*iter)); // 运动方向
+                ++iter;
+                action.reset(new ActionExpression(*iter)); // 运动方式
+                ++iter;
+                distance.reset(new DistanceExpression(*iter)); // 运动距离
+                right.reset(new SentenceExpression(direction, action, distance));
+                expStack.push(make_shared<AndExpression>(left, right));
+            }
+            else
+            {
+                direction.reset(new DirectionExpression(*iter)); // 运动方向
+                ++iter;
+                action.reset(new ActionExpression(*iter)); // 运动方式
+                ++iter;
+                distance.reset(new DistanceExpression(*iter)); // 运动距离
+                expStack.push(make_shared<SentenceExpression>(direction, action, distance));
+            }
+        }
+        shared_ptr<Expression> expression = expStack.top(); // 返回栈顶元素
+        return expression;
+    }
+}
+
+int main()
+{
+#if 0
+    using namespace ns1;
+    map<char, int> varmap;
+    // 下面是给字符串表达式中所有参与运算的变量一个对应的数值
+    varmap.insert(make_pair('a', 7)); // 类似于赋值语句a = 7
+    varmap.insert(make_pair('b', 9)); // 类似于赋值语句b = 9
+    varmap.insert(make_pair('c', 3)); // 类似于赋值语句c = 3
+    varmap.insert(make_pair('d', 2)); // 类似于赋值语句d = 2
+
+    string strExp = "a-b+c+d";                           // 将要求值的字符串表达式
+    shared_ptr<Expression> expression = analyse(strExp); // 调用analyse函数创建语法树
+    int result = expression->interpret(varmap);          // 调用interpret接口求解字符串表达式的结果
+    cout << strExp << " = " << result << endl;           // 输出字符串表达式结果
+#endif
+
+#if 1
+    using namespace ns2;
+    string strExp = "left walk 15 and down run 20";
+    shared_ptr<Expression> expression = analyse(strExp); // 调用analyse函数创建语法树
+    cout << expression->interpret() << endl;
+#endif
+
+    cout << "Over!\n";
+    return 0;
+}
---
+++ 访问者模式.cpp
@@ -0,0 +1,392 @@
+#include <iostream>
+#include <list>
+#include <memory>
+using namespace std;
+
+// 提供一个作用于某对象结构中的各元素的操作表示，便可以在不改变各元素类的前提下定义（扩展）作用于这些元素的新操作
+// 允许一个或多个操作应用于一组对象上，使对象本身和操作解耦
+// 5种角色
+// Visitor（抽象访问者），为对象结构中的元素声明访问接口
+// ConcreteVisitor（具体访问者），实现接口
+// Element（抽象元素），定义Accept方法，形参通常为Visitor指针
+// ConcreteElement（具体元素），实现Accept方法，该方法中调用ConcreteVisitor的访问操作，实现元素操作
+// ObjectStructure（对象结构），元素集合，存放元素对象并提供遍历其内部元素的接口，一般用于同一批元素的相同操作
+
+namespace ns1
+{
+    class Medicine // 药品父类
+    {
+    public:
+        virtual ~Medicine() {}
+        virtual string getMdcName() = 0; // 药品名称
+        virtual float getPrice() = 0;    // 药品总价格，单位：元
+    };
+
+    class M_asplcrp : public Medicine // 药品：阿司匹林肠溶片
+    {
+    public:
+        string getMdcName() override
+        {
+            return "Aspirin enteric-coated tablets";
+        }
+        float getPrice() override
+        {
+            return 46.8f; // 为简化代码，直接给出药品总价而不单独强调药品数量了，比如该药品医生给开了两盒，一盒是23.4，那么这里直接返回两盒的价格
+        }
+    };
+
+    class M_fftdnhsp : public Medicine // 药品：氟伐他汀钠缓释片
+    {
+    public:
+        string getMdcName() override
+        {
+            return "Fluvastatin sodium sustained-release tablets";
+        }
+        float getPrice() override
+        {
+            return 111.3f; // 三盒的价格
+        }
+    };
+
+    class M_dlx : public Medicine // 药品：黛力新
+    {
+    public:
+        string getMdcName() override
+        {
+            return "Deanxit";
+        }
+        float getPrice() override
+        {
+            return 122.0f; // 两盒的价格
+        }
+    };
+
+    class MedicineProc // 针对药品的处理相关类
+    {
+        list<shared_ptr<Medicine>> m_mdclist; // 药品列表，记录着药品单上的所有药品
+    public:
+        // 增加药品到药品列表中
+        void addMedicine(const shared_ptr<Medicine> &p_mdc) { m_mdclist.push_back(p_mdc); }
+
+        // 针对费用缴纳和取药所做的处理动作
+        void procAction(const string &strvisitor) // strvisitor代表拿到了药品单的人，不同的人拿到药品单所要做的处理也不同
+        {
+            if (strvisitor == "Toll collector") // 收费人员要根据药品单向我（患者）收取费用
+            {
+                float totalcost = 0.0f; // 总费用
+                for (auto iter = m_mdclist.begin(); iter != m_mdclist.end(); ++iter)
+                {
+                    float tmpprice = (*iter)->getPrice();
+                    totalcost += tmpprice;
+
+                    cout << "yao: " << (*iter)->getMdcName() << ", price: " << tmpprice << endl;
+                }
+                cout << "total price: " << totalcost << ", Toll collector charges a fee!" << endl;
+            }
+            else if (strvisitor == "Drug taking personnel") // 取药人员要根据药品单为我拿药
+                for (auto iter = m_mdclist.begin(); iter != m_mdclist.end(); ++iter)
+                    cout << "Drug taking personnel give " << (*iter)->getMdcName() << " to me!" << endl;
+            else if (strvisitor == "dietitian")
+                cout << "Nutritionist suggested: match coarse grains with staple foods and eat meat properly!" << endl;
+            else if (strvisitor == "Fitness coach")
+                cout << "The fitness coach suggested: do more aerobic exercises such as jogging (35 minutes), warm up before and after jogging, avoid anxiety, avoid staying up late, and take a bed rest before 22:30!" << endl;
+        }
+    };
+}
+
+namespace ns2
+{
+    class Visitor; // 类前向声明
+    class Medicine // 药品父类
+    {
+    public:
+        virtual ~Medicine() {}
+        virtual void Accept(shared_ptr<Visitor> &pvisitor) = 0; // 这里的形参是访问者父类指针
+    public:
+        virtual string getMdcName() = 0; // 药品名称
+        virtual float getPrice() = 0;    // 药品总价格，单位：元
+    };
+
+    class M_asplcrp : public Medicine // 药品：阿司匹林肠溶片
+    {
+    public:
+        string getMdcName() override { return "Aspirin enteric-coated tablets"; }
+        float getPrice() override { return 46.8f; }
+
+    public:
+        void Accept(shared_ptr<Visitor> &pvisitor) override;
+    };
+
+    class M_fftdnhsp : public Medicine // 药品：氟伐他汀钠缓释片
+    {
+    public:
+        string getMdcName() override { return "Fluvastatin sodium sustained-release tablets"; }
+        float getPrice() override { return 111.3f; }
+
+    public:
+        void Accept(shared_ptr<Visitor> &pvisitor) override;
+    };
+
+    class M_dlx : public Medicine // 药品：黛力新
+    {
+    public:
+        string getMdcName() override { return "Deanxit"; }
+        float getPrice() override { return 122.0f; }
+
+    public:
+        void Accept(shared_ptr<Visitor> &pvisitor) override;
+    };
+
+    class Visitor // 访问者父类
+    {
+    public:
+        virtual ~Visitor() {}
+
+        virtual void Visit_elm_asplcrp(M_asplcrp *pelem) = 0;   // 访问元素：阿司匹林肠溶片
+        virtual void Visit_elm_fftdnhsp(M_fftdnhsp *pelem) = 0; // 访问元素：氟伐他汀钠缓释片
+        virtual void Visit_elm_dlx(M_dlx *pelem) = 0;           // 访问元素：黛力新
+
+        ////以下几个接口的名字都叫Visit（成员函数重载）
+        // virtual void Visit(M_asplcrp* pelem) = 0;   //访问元素：阿司匹林肠溶片
+        // virtual void Visit(M_fftdnhsp* pelem) = 0;  //访问元素：氟伐他汀钠缓释片
+        // virtual void Visit(M_dlx* pelem) = 0;       //访问元素：黛力新
+    };
+
+    class Visitor_SFRY : public Visitor // 收费人员访问者子类
+    {
+        float m_totalcost = 0.0f; // 总费用
+    public:
+        void Visit_elm_asplcrp(M_asplcrp *pelem) override
+        {
+            float tmpprice = pelem->getPrice();
+            cout << "yao: " << pelem->getMdcName() << ", price: " << tmpprice << endl;
+            m_totalcost += tmpprice;
+        }
+        void Visit_elm_fftdnhsp(M_fftdnhsp *pelem) override
+        {
+            float tmpprice = pelem->getPrice();
+            cout << "yao: " << pelem->getMdcName() << ", price: " << tmpprice << endl;
+            m_totalcost += tmpprice;
+        }
+        void Visit_elm_dlx(M_dlx *pelem) override
+        {
+            float tmpprice = pelem->getPrice();
+            cout << "yao: " << pelem->getMdcName() << ", price: " << tmpprice << endl;
+            m_totalcost += tmpprice;
+        }
+
+        // 返回总费用
+        float getTotalCost() const { return m_totalcost; }
+    };
+
+    class Visitor_QYRY : public Visitor // 取药人员访问者子类
+    {
+    public:
+        void Visit_elm_asplcrp(M_asplcrp *pelem) override
+        {
+            cout << "Drug taking personnel give " << pelem->getMdcName() << " to me!" << endl;
+        }
+        void Visit_elm_fftdnhsp(M_fftdnhsp *pelem) override
+        {
+            cout << "Drug taking personnel give " << pelem->getMdcName() << " to me!" << endl;
+        }
+        void Visit_elm_dlx(M_dlx *pelem) override
+        {
+            cout << "Drug taking personnel give " << pelem->getMdcName() << " to me!" << endl;
+        }
+    };
+
+    class Visitor_YYS : public Visitor // 营养师访问者子类
+    {
+    public:
+        void Visit_elm_asplcrp(M_asplcrp *pelem) override
+        {
+            cout << "The nutritionist suggested: Eating more coarse grains and less oil can effectively prevent blood clots!" << endl;
+        }
+        void Visit_elm_fftdnhsp(M_fftdnhsp *pelem) override
+        {
+            cout << "The nutritionist suggested: Eating more mushrooms, onions, and kiwifruit can effectively reduce blood lipids!" << endl;
+        }
+        void Visit_elm_dlx(M_dlx *pelem) override
+        {
+            cout << "The nutritionist suggested: Go out more to breathe fresh air, get more sunshine, go to more crowded places, and keep an optimistic and cheerful mood!" << endl;
+        }
+    };
+
+    // 各个药品子类Accept方法的实现体代码
+    void M_asplcrp::Accept(shared_ptr<Visitor> &pvisitor)
+    {
+        pvisitor->Visit_elm_asplcrp(this);
+    }
+    void M_fftdnhsp::Accept(shared_ptr<Visitor> &pvisitor)
+    {
+        pvisitor->Visit_elm_fftdnhsp(this);
+    }
+    void M_dlx::Accept(shared_ptr<Visitor> &pvisitor)
+    {
+        pvisitor->Visit_elm_dlx(this);
+    }
+
+    class ObjectStructure // 对象结构
+    {
+        list<shared_ptr<Medicine>> m_mdclist; // 药品列表
+    public:
+        // 增加药品到药品列表中
+        void addMedicine(const shared_ptr<Medicine> &p_mdc)
+        {
+            m_mdclist.push_back(p_mdc);
+        }
+
+        void procAction(shared_ptr<Visitor> &pvisitor)
+        {
+            for (auto iter = m_mdclist.begin(); iter != m_mdclist.end(); ++iter)
+                (*iter)->Accept(pvisitor);
+        }
+    };
+}
+
+namespace ns3
+{
+    class Medicine; // 类前向声明
+    class Visitor   // 访问者父类
+    {
+    public:
+        virtual ~Visitor() {}
+        virtual void Visit(Medicine *pelem) = 0;
+    };
+
+    class Medicine // 药品父类
+    {
+    public:
+        virtual ~Medicine() {}
+        virtual void Accept(shared_ptr<Visitor> &pvisitor) { pvisitor->Visit(this); }
+
+    public:
+        virtual string getMdcName() = 0; // 药品名称
+        virtual float getPrice() = 0;    // 药品总价格，单位：元
+    };
+
+    class M_asplcrp : public Medicine // 药品：阿司匹林肠溶片
+    {
+    public:
+        string getMdcName() override { return "Aspirin enteric-coated tablets"; }
+        float getPrice() override { return 46.8f; }
+    };
+
+    class M_fftdnhsp : public Medicine // 药品：氟伐他汀钠缓释片
+    {
+    public:
+        string getMdcName() override { return "Fluvastatin sodium sustained-release tablets"; }
+        float getPrice() override { return 111.3f; }
+    };
+
+    class M_dlx : public Medicine // 药品：黛力新
+    {
+    public:
+        string getMdcName() override { return "Deanxit"; }
+        float getPrice() override { return 122.0f; }
+    };
+
+    class Visitor_SFRY : public Visitor // 收费人员访问者子类
+    {
+        float m_totalcost = 0.0f; // 总费用
+    public:
+        void Visit(Medicine *m) override
+        {
+            float tmpprice = m->getPrice();
+            m_totalcost += tmpprice;
+
+            cout << "yao: " << m->getMdcName() << ", price: " << tmpprice << endl;
+        }
+
+        float getTotalCost() const { return m_totalcost; } // 返回总费用
+    };
+
+    class Visitor_QYRY : public Visitor // 取药人员访问者子类
+    {
+    public:
+        void Visit(Medicine *m) override
+        {
+            cout << "Drug taking personnel give " << m->getMdcName() << " to me!" << endl;
+        }
+    };
+
+    class Visitor_YYS : public Visitor // 营养师访问者子类
+    {
+    public:
+        void Visit(Medicine *m) override
+        {
+            cout << "The nutritionist suggested: Eating more coarse grains and less oil can effectively prevent blood clots!" << endl;
+            cout << "The nutritionist suggested: Eating more mushrooms, onions, and kiwifruit can effectively reduce blood lipids!" << endl;
+            cout << "The nutritionist suggested: Go out more to breathe fresh air, get more sunshine, go to more crowded places, and keep an optimistic and cheerful mood!" << endl;
+        }
+    };
+
+    class ObjectStructure // 对象结构
+    {
+        list<shared_ptr<Medicine>> m_mdclist; // 药品列表
+    public:
+        // 增加药品到药品列表中
+        void addMedicine(const shared_ptr<Medicine> &p_mdc)
+        {
+            m_mdclist.push_back(p_mdc);
+        }
+
+        void procAction(shared_ptr<Visitor> &pvisitor)
+        {
+            for (auto iter = m_mdclist.begin(); iter != m_mdclist.end(); ++iter)
+                (*iter)->Accept(pvisitor);
+        }
+    };
+}
+
+int main()
+{
+#if 0
+    using namespace ns1;
+    shared_ptr<Medicine> pm1(new M_asplcrp());  // 药品阿司匹林肠溶片
+    shared_ptr<Medicine> pm2(new M_fftdnhsp()); // 药品氟伐他汀钠缓释片
+    shared_ptr<Medicine> pm3(new M_dlx());      // 药品黛力新
+
+    MedicineProc mdcprocobj;
+    mdcprocobj.addMedicine(pm1);
+    mdcprocobj.addMedicine(pm2);
+    mdcprocobj.addMedicine(pm3);
+    mdcprocobj.procAction("Toll collector");
+    mdcprocobj.procAction("Drug taking personnel");
+#endif
+
+#if 1
+    //using namespace ns2;
+    using namespace ns3;
+    shared_ptr<Visitor> visitor_sf(new Visitor_SFRY()); // 收费人员访问者子类，里面承载着向我（患者）收费的算法
+    shared_ptr<Medicine> mdc_asplcrp(new M_asplcrp());
+    shared_ptr<Medicine> mdc_fftdnhsp(new M_fftdnhsp());
+    shared_ptr<Medicine> mdc_dlx(new M_dlx());
+
+    // 各个元素子类调用Accept接受访问者的访问，就可以实现访问者要实现的功能
+    mdc_asplcrp->Accept(visitor_sf);  // 累加“阿司匹林肠溶片”的价格
+    mdc_fftdnhsp->Accept(visitor_sf); // 累加“氟伐他汀钠缓释片”的价格
+    mdc_dlx->Accept(visitor_sf);      // 累加“黛力新”的价格
+    cout << "total price: " << dynamic_pointer_cast<Visitor_SFRY>(visitor_sf)->getTotalCost() << ", the toll collector charged me!" << endl;
+
+    shared_ptr<Visitor> visitor_qy(new Visitor_QYRY()); // 取药人员访问者子类，里面承载着向我发放药品的算法
+    mdc_asplcrp->Accept(visitor_qy);                    // 我取得“阿司匹林肠溶片”
+    mdc_fftdnhsp->Accept(visitor_qy);                   // 我取得“氟伐他汀钠缓释片”
+    mdc_dlx->Accept(visitor_qy);                        // 我取得“黛力新”
+
+    shared_ptr<Visitor> visitor_yys(new Visitor_YYS()); // 营养师访问者子类，里面承载着为我配置营养餐的算法
+    mdc_asplcrp->Accept(visitor_yys);                   // 营养师针对治疗预防血栓药“阿司匹林肠溶片”给出的对应的营养餐建议
+    mdc_fftdnhsp->Accept(visitor_yys);                  // 营养师针对降血脂药“氟伐他汀钠缓释片”给出的对应的营养餐建议
+    mdc_dlx->Accept(visitor_yys);                       // 营养师针对治疗神经紊乱药“黛力新”给出的对应的营养餐建议
+
+    shared_ptr<ObjectStructure> objstruc(new ObjectStructure());
+    objstruc->addMedicine(mdc_asplcrp);
+    objstruc->addMedicine(mdc_fftdnhsp);
+    objstruc->addMedicine(mdc_dlx);
+    objstruc->procAction(visitor_yys); // 将一个访问者对象（visitor_yys）应用到一批元素上，以实现对一批元素进行同一种（营养方面的）操作
+#endif
+
+    cout << "Over!\n";
+    return 0;
+}
